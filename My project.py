f5132c1aa1f98c0e8b8803cf680e45637a23a8bc
vitalyevich348 <vsartakov31@gmail.com>
2024-11-18 15:18:40 +0500

    Add files via upload

---
+++ My project.py
@@ -0,0 +1,225 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+17
+2 True
+3 True
+4 True
+Это простое число
+>>> 
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+101
+2 True
+3 True
+4 True
+5 True
+6 True
+7 True
+8 True
+9 True
+10 True
+Это простое число
+>>> 
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+18
+Это сложное число
+>>> 
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+12
+2 True
+3 False
+4 False
+5 False
+6 False
+7 False
+8 False
+9 False
+10 False
+11 False
+Это сложное число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+12
+2 True
+Это сложное число
+13
+13
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+13
+2 True
+3 True
+4 True
+5 True
+6 True
+7 True
+8 True
+9 True
+10 True
+11 True
+12 True
+Это простое число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+13
+2 True
+3 True
+4 True
+5 True
+6 True
+Это простое число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+12
+2 True
+3 False
+4 False
+5 False
+6 False
+Это сложное число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+25
+2 True
+3 True
+4 True
+5 True
+6 False
+7 False
+8 False
+9 False
+10 False
+11 False
+12 False
+Это сложное число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+17
+2 True
+3 True
+4 True
+5 True
+6 True
+7 True
+8 True
+Это простое число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+101
+Это простое число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+102
+Это сложное число
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+32
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+38
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:казак
+Слово палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:дед
+Слово палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:ага
+Слово палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:шалаш
+Слово палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:заказ
+Слово палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:конок
+Слово не палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:заз
+Слово не палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:винив
+Слово не палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:учу
+Слово не палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите слово:кик
+Слово не палиндромное!
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите число 7
+2
+3
+4
+5
+6
+7
+5040
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Введите число 7
+2
+3
+4
+5
+6
+7
+7 ! = 5040
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+5
+1 1 2 3 5 
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+20
+1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[-800, -44, -19, 7, 54, 76, 90]
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+Corporation IMAX
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+('cinema', 'starshak', 'building', 'bloger')
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[-800, -44, -19, 7, 54, 76, 90]
+[7, -19, 54, -800, 76, 90, -44]
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[-800, -44, -19, 7, 54, 76, 90]
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[' ', 'A', 'C', 'I', 'M', 'X', 'a', 'i', 'n', 'o', 'o', 'o', 'p', 'r', 'r', 't']
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[12, 35, 44, 65, 67, 77, 90]
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[86, 76, 54, 46, 13]
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+XAMI noitaroproC
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+77
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77
+
+== RESTART: C:/Users/vlad_/AppData/Local/Programs/Python/Python313/Задания.py ==
+[3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
